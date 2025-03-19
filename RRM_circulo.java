e447a9b54e8a204141dbcbb1e2c96155e0df0b7d
DE LA ROSA MUÑOZ <raul.de1@isepceu.es>
2025-03-19 19:38:38 +0100

    RRM_Commit_de_la_Primera_Clase_circulo

---
+++ RRM_circulo.java
@@ -0,0 +1,43 @@
+public class RRM_circulo {
+    // Clase
+    int id = 1;
+    double radio = 10.1;
+    String color = "Naranja";
+
+    public RRM_circulo(Integer id, double radio, String color) {
+        // Constructor
+        this.id = id;
+        this.radio = radio;
+        this.color = color;
+    }
+
+    // Getter y Setter
+    public Integer getid() {
+        return id;
+    }
+
+    public double getradio() {
+        return radio;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+
+    public void setid(Integer id) {
+        this.id = id;
+    }
+
+    public void setradio(double radio) {
+        this.radio = radio;
+    }
+
+    public void setcolor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "RRM_circulo [id=" + id + ", radio=" + radio + ", color=" + color + "]";
+    }
+}
